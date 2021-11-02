e2ebc7f7ec45a3b65eb40b2ec1bfa62682f8abdb
Partho Bhowmick <p.s.bhowmick@gmail.com>
2021-11-02 11:10:58 -0500

    complete

--- read_english_dictionary.py
+++ read_english_dictionary.py
@@ -1,29 +1,22 @@
+#!/usr/bin/env python3
+
 import argparse
 from typing import Iterator, List, Set
 
-from itertools import *
-from itertools import filterfalse
+from itertools import filterfalse, combinations
 
 
-def load_words(min_length: int, valid_words=None) -> Iterator[str]:
+def filter_by_letterset(letters: Set[str], words_set: Iterator[str]) \
+        -> Iterator[str]:
     """
-    Load all the words in the dictionary and automatically rejects words of
-    Returns:
-
-    """
-    return filterfalse(lambda x: len(x) < min_length, valid_words)
-
-
-def filter_by_letterset(letters: Set[str], words_set: Iterator[str]) -> Iterator[str]:
-    """
-    Filters the words in the dictionary by the letter-set. For example,
-
+    Filters the words in the dictionary by the letter-set. So, the set
+    {'h', 'o', 'w'} will match 'how' and 'who' but not 'whom'
     Args:
-        letters:
-        words_set:
+        letters: The set of letters to use to filter against the diction
+        words_set: The supplied dictionary to filter
 
     Returns:
-
+        An iterator for words that matched the letter-set
     """
     sz: int = len(letters)
     words_subset: Iterator[str] = filterfalse(lambda x: len(x) < sz, words_set)
@@ -32,34 +25,36 @@ def filter_by_letterset(letters: Set[str], words_set: Iterator[str]) -> Iterator
 
 def combos(letters: str) -> List[Set[str]]:
     """
-
+    Get all combinations of the 7-letter set, the smallest set will be of
+    size 4 and the largest of size 7
     Args:
-        letters:
+        letters: The string of 7-letters provided, the first of which is compulsory
 
     Returns:
-
+        A list of all combinations of the 7-letter set, the smallest set will be of
+    size 4 and the largest of size 7
     """
-    x: list[set[str]] = []
-    compulsory: set[str] = {letters[0]}
+    x: List[Set[str]] = []
+    compulsory: Set[str] = {letters[0]}
     for j in range(3, 7):
-        x.extend([set(j).union(compulsory) for j in combinations(letters[1:], j)])
+        x.extend([set(x).union(compulsory)
+                  for x in combinations(letters[1:], j)])
     return x
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='7 letters please.')
     parser.add_argument('letters', type=str,
-                        help='7-letter starting with the cumpulsory one ')
+                        help='7-letter starting with the compulsory one ')
     args = parser.parse_args()
 
     ctr = 0
     with open('words_alpha.txt') as word_file:
-        dictionary: List[str] = word_file.read().split()
+        dictionary: List[str] = [x for x in word_file.read().split() if len(x) >= 4]
 
     for c in combos(args.letters):
-        english_words = load_words(len(c), dictionary)
+        words_found = filter_by_letterset(c, dictionary)
         print(c)
-        words_found = filter_by_letterset(c, english_words)
         for w in words_found:
             ctr += 1
             print(f"{ctr}\t\t{w}")
